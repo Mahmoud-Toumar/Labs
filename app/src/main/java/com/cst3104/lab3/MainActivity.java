407692f40cd0326b79c121affd15cd8c6392f753
Mahmoud <mtoumar01@gmail.com>
2023-10-02 13:02:35 -0400

    Final Commit

--- app/src/main/java/com/cst3104/lab3/MainActivity.java
+++ app/src/main/java/com/cst3104/lab3/MainActivity.java
@@ -21,16 +21,18 @@ public class MainActivity extends AppCompatActivity {
         imgView = findViewById(R.id.flagview);
         sw = findViewById(R.id.spin_switch);
         sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if(isChecked){
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+
+            }
+            else imgView.clearAnimation();
         });
     }
 
-    {
-        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF, 0.5f);
-        rotate.setDuration(5000);
-        rotate.setRepeatCount(Animation.INFINITE);
-        rotate.setInterpolator(new LinearInterpolator());
-        imgView.startAnimation(rotate);
-    }
 
 }
